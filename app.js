5191d305b792cd62d159366d30c3f00bea230de9
AGAIN by LAKRAN <againbylakran@MacBook-Pro-van-Wendy.local>
2018-03-15 13:54:39 +0100

    Added file structure and some basic packages

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.post('/send', (req, res) => {
+    let output = `
+        <h1>Contact Form AGAIN.nl</h1>
+        <h2>Details:</h2>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: <a href="mailto:${req.body.email}">${req.body.email}</a></li>
+            <li>Subject: ${req.body.subject}</li>
+        </ul>
+        <h4 style="margin-bottom:0;">Message:</h4> 
+        <p>${req.body.msg}</p>
+    `;
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.sendgrid.net",
+        secure: false,
+        port: 25,
+        auth: {
+            user: process.env.SENDGRID_USER,
+            pass: process.env.SENDGRID_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    
+    let HelperOptions = {
+        from: '"Contact Form AGAIN website" <wendy.dimmendaal@again.nl>',
+        to: 'wendy.dimmendaal@again.nl',
+        subject: 'Hello World',
+        text: 'Test 123',
+        html: output
+    };
+    
+    transporter.sendMail(HelperOptions, (error, info) => {
+        if(error) {
+            res.render('error', {errorMsg: error, defaultLayout: 'simple'});
+        }
+        res.redirect('/?form=send');
+    });
+})
+
+var port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log('Server started...');
+});
