85707d5f0dbaa527123162c9cf331ec6acf09cf5
AGAIN by LAKRAN <againbylakran@MacBook-Pro-van-Wendy.local>
2018-03-20 16:13:35 +0100

    Added some partials

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const i18n = require('i18n-express');
 
 const app = express();
 
+// Setup Firebase
+const config = {
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    databaseURL: process.env.FB_DATABASE_URL,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_PROCESS_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
+}
+
 // Setup language
 app.use(i18n({
     translationsPath: path.join(__dirname, 'i18n'),
